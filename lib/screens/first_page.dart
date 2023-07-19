af0a35f7455a6a93de21182cf968645af493a3b4
Anupama shammy <shammyanupama007@gmail.com>
2023-07-19 21:18:13 +0530

    first commit

---
+++ lib/screens/first_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WelcomeMyREstaUrant extends StatefulWidget {
+  const WelcomeMyREstaUrant({super.key});
+
+  @override
+  State<WelcomeMyREstaUrant> createState() => _WelcomeMyREstaUrantState();
+}
+
+class _WelcomeMyREstaUrantState extends State<WelcomeMyREstaUrant> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        
+
+      ),
+
+    );
+  }
+}
